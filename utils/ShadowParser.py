04a2d93c800c4c23108a283b1e4ad9b47a8333c5
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-11-07 16:17:38 -0800

    Added a shadow parser which parses the shadow file (duh)

--- utils/ShadowParser.py
+++ utils/ShadowParser.py
@@ -1,5 +1,4 @@
 
-
 class ShadowParser:
 
     def __init__(self):
@@ -7,4 +6,25 @@ class ShadowParser:
 
     def parse(self, filepath):
         print(f"Parsing Shadow File: {filepath}")
+        with open("/etc/shadow", "r") as f:
+            print(f.read())
+
+class ShadowFile:
+    """
+        A class representing a parsed shadow file.
+        Simply contains a list of ShadowFileEntry objects.
+    """
+    def __init__(self):
+        pass
+
+
+class ShadowFileEntry:
+
+    def __init__(self, entry: str = ""):
+        # Parse the entry and fill in
+        # the instance variables.
+        if entry == "":
+            pass
+        else:
+            pass
 
