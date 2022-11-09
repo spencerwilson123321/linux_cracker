9cfe72b277664201ab5320a49f73efe073333bd1
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-11-09 12:08:49 -0800

    modified

--- utils/Shadow.py
+++ utils/Shadow.py
@@ -46,7 +46,6 @@ class ShadowFileEntry:
                 self.password_hash = ""
             else:
                 extra, algorithm, extra2, salt, password_hash = tokens[1].split("$")
-                print(tokens[1].split("$"))
                 self.algorithm = algorithm
                 self.salt = salt
                 self.password_hash = password_hash
